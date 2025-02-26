0e45b2cdb1d6c456b21f7d520526299567e57e8c
Andey.Malinin <Andre.Malinin@x5.ru>
2025-02-27 01:34:33 +0300

    Initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,63 @@
+import { useCallback, useEffect, useState } from 'react'
+import './App.css'
+import verbs from './verbs.json'
+import Alert from '@mui/material/Alert';
+import { Snackbar, SnackbarCloseReason } from '@mui/material';
+import { useSnackbar } from 'notistack';
+
+enum TYPE_OF_VERB {
+  "INFINITIVE" = "infinite",
+  "GERUND" = "gerund"
+}
+
+enum TYPE_OF_ANSWER {
+  "CURRECT" = "success",
+  "UNCURRECT" = "error"
+}
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
+function App() {
+  const [type, setType] = useState<TYPE_OF_VERB | null>(null)
+  const [index, setIndex] = useState<number>(0)
+  const { enqueueSnackbar } = useSnackbar();
+
+  const update = useCallback(() => {
+    setType(getRandomInt(2) === 1 ? TYPE_OF_VERB.GERUND : TYPE_OF_VERB.INFINITIVE);
+    setIndex(getRandomInt(verbs.gerund.length));
+  }, [])
+
+  useEffect(() => {
+    update()
+  }, [update])
+
+  const handleClick = (value: TYPE_OF_VERB) => {
+    if (type === value) {
+      enqueueSnackbar('Правильно :)', { variant: "success" });
+    } else {
+      enqueueSnackbar('Неправильно :(', { variant: "error" });
+    }
+    update();
+  }
+
+  if (!type) return null;
+
+  return (
+    <>
+      <h1>Memorizator</h1>
+      <h4>{Object.keys(verbs[type][index])}</h4>
+      <div className="card">
+        <button onClick={() => handleClick(TYPE_OF_VERB.INFINITIVE)}>
+          Infinitive
+        </button>
+        <button onClick={() => handleClick(TYPE_OF_VERB.GERUND)}>
+          Gerund
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './app/App'
+import { SnackbarProvider } from 'notistack'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+     <SnackbarProvider maxSnack={3} autoHideDuration={2000}>
+      <App />
+     </SnackbarProvider>
+  </StrictMode>,
+)
